9ff923a58c2a9e2604baf189a06417db654cfc43
Pessoa <ppessoa@asu.edu>
2025-04-12 16:17:44 -0700

    setup_pip

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+print( find_packages() )
+setup(
+    name="repop",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=["torch",
+                      "numpy",
+                      "matplotlib",
+                      "scikit-learn"],
+    author="Pedro Pessoa",
+    author_email="ppessoa@asu.edu",
+    description="REPOP - Library for REconstructing bacterial POpulations from Plate counts.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/PessoaP/REPOP",
+    license="LICENSE",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
+    ],
+)
